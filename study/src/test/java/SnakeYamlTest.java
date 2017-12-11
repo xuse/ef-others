59bb9352437538302f9b156a5c1545105c3fadc0
xuse <mr.jiyi@gmail.com>
2017-12-11 17:09:05 +0800

    增加Yaml测试和比较心得。

--- study/src/test/java/SnakeYamlTest.java
+++ study/src/test/java/SnakeYamlTest.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
 import java.util.Map;
 
-import javax.xml.bind.JAXB;
-
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 
 import com.alibaba.fastjson.JSON;
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
 
-import jef.tools.X;
+import jef.tools.IOUtils;
 
 public class SnakeYamlTest {
 
@@ -17,8 +17,16 @@ public class SnakeYamlTest {
 		Yaml yaml = new Yaml();
 		Map me = yaml.load(this.getClass().getResource("/test_snake.yaml").openStream());
 		System.out.println(JSON.toJSONString(me));
-		
-		//X.print(X.putBean(X.$new(), me));
+
+		// X.print(X.putBean(X.$new(), me));
+	}
+
+	@Test
+	public void yamlBeanTest() throws IOException {
+		YamlReader reader = new YamlReader(IOUtils.getReader(this.getClass(),"/test_snake.yaml","UTF-8"));
+		Map demo = reader.read(Map.class);
+		reader.close();
+		System.out.println(JSON.toJSONString(demo));
 	}
 
 }
